456fe192bf16dcd43b23e163d8695bfa7f5a57c9
micallej <43765335+micallej@users.noreply.github.com>
2018-12-05 21:12:36 -0800

    Add files via upload

--- final.py
+++ final.py
@@ -65,29 +65,7 @@ def PCAPlot(X, Y, n):
     
     print('The  values represent the importance of each of the component axes in classifying the dataset:')
     print('explained variance: ', pca.explained_variance_)	
-    
-def visualizeClassifier(model, X, y, ax=None, cmap='rainbow'):
 	
-    ax = ax or matplotlib.pyplot.gca()
-    # Plot the training points
-    ax.scatter(X[:, 0], X[:, 1], c=y, s=30, cmap=cmap,
-               clim=(y.min(), y.max()), zorder=3)
-    ax.axis('tight')
-    ax.axis('off')
-    xlim = ax.get_xlim()
-    ylim = ax.get_ylim()
-    
-    # fit the estimator
-    model.fit(X, y)
-    xx, yy = numpy.meshgrid(numpy.linspace(*xlim, num=200),numpy.linspace(*ylim, num=200))
-    Z = model.predict(numpy.c_[xx.ravel(), yy.ravel()]).reshape(xx.shape)
-
-    # Create a color plot with the results
-    n_classes = len(numpy.unique(y))
-    contours = ax.contourf(xx, yy, Z, alpha=0.3,levels=numpy.arange(n_classes + 1) - 0.5,cmap=cmap, clim=(y.min(), y.max()),zorder=1)
-
-    ax.set(xlim=xlim, ylim=ylim)
-		
 def RFScore(XTrain, XTest, YTrain, YTest, n):
 	#perform random forrst learning and score result
     scores = []
